62fb791cd407782b0211abc4eb5f7289567dfdf0
Anton <anton@adc.sh>
2019-07-05 11:30:24 +0300

    c1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 ## 5 July 2019
 
+### [1.0.0](https://github.com/idiocc/form/compare/v0.0.0-pre...v1.0.0)
+
+- [package] Publish `v1` of the package.
+
 ### 0.0.0-pre
 
 - Create `@multipart/form` with _[`My New Package`](https://mnpjs.org)_
--- build/index.js
+++ build/index.js
@@ -1,34 +1,95 @@
-const { debuglog } = require('util');
+const { readBuffer } = require('@wrote/read');
 
-const LOG = debuglog('@multipart/form')
+class Form {
+  /**
+   * Creates a new form instance that maintains a buffer of key-value pairs and files separated by a boundary.
+   * @param {Config} [opts] Options for the constructor.
+   * @param {string} [opts.boundary="u2KxIV5yF1y+xUspOQCCZopaVgeV6Jxihv35XQJmuTx8X3sh"] The hard-coded boundary for the requests. Default `u2KxIV5yF1y+xUspOQCCZopaVgeV6Jxihv35XQJmuTx8X3sh`.
+   */
+  constructor(opts = {}) {
+    const {
+      boundary = 'u2KxIV5yF1y+xUspOQCCZopaVgeV6Jxihv35XQJmuTx8X3sh',
+    } = opts
+    /**
+     * @type {!Array<!Buffer>}
+     */
+    this._data = []
+    this._boundary = boundary
+  }
+  /**
+   * @param {string} path The path to the file.
+   * @param {string} name The name of the field.
+   * @param {AddFileOptions} [options] Options for adding files.
+   * @param {string} [options.type="application/octet-stream"] The _Content-Type_ description. Default `application/octet-stream`.
+   * @param {boolean} [options.noCache=false] Whether to not cache read files. Default `false`.
+   * @param {string} [options.filename] The `filename` property for _Content-Disposition_ description. By default, will be same as the `path` argument.
+   */
+  async addFile(path, name, options = {}) {
+    const {
+      contentType = 'application/octet-stream',
+      noCache = false,
+      filename = path,
+    } = options
+    let file
+    if (path in formCache || noCache) {
+      file = formCache[path]
+    } else {
+      file = await readBuffer(path)
+      if (!noCache) formCache[path] = file
+    }
+    const disposition = [
+      'form-data',
+      `name="${name}"`,
+      `filename="${filename}"`,
+    ].join('; ')
+    this.writeLine(`\r\n--${this.boundary}`)
+    this.writeLine(`Content-Disposition: ${disposition}`)
+    this.writeLine(`Content-Type: ${contentType}`)
+    this.writeLine()
+    this._data.push(file)
+  }
+  writeLine(line) {
+    if (line) this._data.push(typeof line == 'string' ? Buffer.from(line) : line)
+    this._data.push(Buffer.from('\r\n'))
+  }
+  /**
+   * The boundary.
+   */
+  get boundary() {
+    return this._boundary
+  }
+  /**
+   * The complete data as a string.
+   */
+  get data() {
+    this.writeLine(`\r\n--${this.boundary}--`)
 
-/**
- * A Node.JS Class To Create Multipart/Form-Data Requests With Files and Fields.
- * @param {_@multipart/form.Config} [config] Options for the program.
- * @param {boolean} [config.shouldRun=true] A boolean option. Default `true`.
- * @param {string} config.text A text to return.
- */
-               async function form(config = {}) {
-  const {
-    shouldRun = true,
-    text,
-  } = config
-  if (!shouldRun) return
-  LOG('@multipart/form called with %s', text)
-  return text
+    return Buffer.concat(this._data).toString()
+  }
+  /**
+   * Adds a key-value pair to the form.
+   * @param {string} key The key to add.
+   * @param {string} value The value for the key.
+   */
+  addSection(key, value) {
+    this.writeLine(`\r\n--${this.boundary}`)
+    this.writeLine(`Content-Disposition: form-data; name="${key}"`)
+    this.writeLine()
+    this._data.push(Buffer.from(value))
+  }
 }
 
-/* documentary types/index.xml */
+const formCache = {}
+
+/* typal types/index.xml */
 /**
- * @suppress {nonStandardJsDocs}
- * @typedef {_@multipart/form.Config} Config Options for the program.
- */
-/**
- * @suppress {nonStandardJsDocs}
- * @typedef {Object} _@multipart/form.Config Options for the program.
- * @prop {boolean} [shouldRun=true] A boolean option. Default `true`.
- * @prop {string} text A text to return.
+ * @typedef {Object} Config Options for the constructor.
+ * @prop {string} [boundary="u2KxIV5yF1y+xUspOQCCZopaVgeV6Jxihv35XQJmuTx8X3sh"] The hard-coded boundary for the requests. Default `u2KxIV5yF1y+xUspOQCCZopaVgeV6Jxihv35XQJmuTx8X3sh`.
+ * @typedef {Object} AddFileOptions Options for adding files.
+ * @prop {string} [type="application/octet-stream"] The _Content-Type_ description. Default `application/octet-stream`.
+ * @prop {boolean} [noCache=false] Whether to not cache read files. Default `false`.
+ * @prop {string} [filename] The `filename` property for _Content-Disposition_ description. By default, will be same as the `path` argument.
  */
 
 
-module.exports = form
+module.exports = Form
